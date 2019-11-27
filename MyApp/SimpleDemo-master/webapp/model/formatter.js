c59a797c4681f46babb6fbee8dec1c68d8aee2e4
Tazlaoanu <ST073494@cerner.net>
2019-11-27 15:47:22 +0200

    code refactoring

--- MyApp/SimpleDemo-master/webapp/model/formatter.js
+++ MyApp/SimpleDemo-master/webapp/model/formatter.js
@@ -1,35 +1,26 @@
 sap.ui.define([], function() {
 	'use strict';
 
+	const type = {
+		POUND: '£',
+		EURO: '€',
+		DOLLAR: '$'
+	};
 	return {
 		currencyText: function(sCurrency) {
-			const pound = '£';
-			const euro = '€';
-			const dollar = '$';
-
 			switch (sCurrency) {
 				case 'EUR':
-					return euro;
+					return type.EURO;
 				case 'USD':
-					return dollar;
+					return type.DOLLAR;
 				case 'GBP':
-					return pound;
+					return type.POUND;
 				default:
 					return sCurrency;
 			}
 		},
 		dateTimeText: function(productDate) {
 			let date = new Date(productDate);
-			console.log(date);
-			// let date = productDate.split(' ')[0];
-			// let time = productDate.split(' ')[1];
-			// let dd = date.split('/')[1];
-			// let mm = date.split('/')[2];
-			// let yyyy = date.split('/')[0];
-			// let hh = time.split(':')[0];
-			// let min = time.split(':')[1];
-			// let ss = time.split(':')[2];
-
 			let yyyy = date.getFullYear();
 			let mm = date.getMonth() + 1;
 			let dd = date.getDate();
